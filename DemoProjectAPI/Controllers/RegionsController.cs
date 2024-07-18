0581e79db69b37162aedf8e34ee91b099a3c1ca0
Srikanth Rallabandi <codewithsrikanth@hotmail.com>
2024-07-18 21:18:57 +0530

    Insert Region by uisng DTO

--- DemoProjectAPI/Controllers/RegionsController.cs
+++ DemoProjectAPI/Controllers/RegionsController.cs
@@ -1,4 +1,5 @@
 ﻿using DemoProjectAPI.Data;
+using DemoProjectAPI.Models.Domain;
 using DemoProjectAPI.Models.DTO;
 using Microsoft.AspNetCore.Mvc;
 
@@ -52,6 +53,33 @@ namespace DemoProjectAPI.Controllers
             {
                 return Ok(region);
             }
+        }
+
+        //Post
+        [HttpPost]
+        public IActionResult Create([FromBody] AddRegionRequestDto addRegionRequestDto)
+        {
+            //Map or Convert DTO to Domain Model
+            var regionModel = new Region()
+            {
+                Code = addRegionRequestDto.Code,
+                Name = addRegionRequestDto.Name,
+                RegionImageUrl = addRegionRequestDto.RegionImageUrl,
+            };
+
+            //Use Domain Model and Store the data
+            _context.Regions.Add(regionModel);
+            _context.SaveChanges();
+
+            var regionDto = new RegionDto()
+            {
+                Id = regionModel.Id, 
+                Name = regionModel.Name,
+                RegionImageUrl = regionModel.RegionImageUrl,
+                Code = regionModel.Code
+            };
+
+            return CreatedAtAction(nameof(GetById),new { id= regionDto.Id },regionDto);
         }
     }
 }
