2ecc2ea361fa4b6269dc21d005ecf9edd545009b
Srikanth Rallabandi <codewithsrikanth@hotmail.com>
2024-07-24 19:09:37 +0530

    Seeding data into Regions and Difficulties using EF Core

--- DemoProjectAPI/Data/WalksDbContext.cs
+++ DemoProjectAPI/Data/WalksDbContext.cs
@@ -5,11 +5,84 @@ namespace DemoProjectAPI.Data
 {
     public class WalksDbContext : DbContext
     {
-        public WalksDbContext(DbContextOptions dbContextOptions):base(dbContextOptions)
-        {            
+        public WalksDbContext(DbContextOptions dbContextOptions) : base(dbContextOptions)
+        {
         }
         public DbSet<Region> Regions { get; set; }
         public DbSet<Difficulty> Difficulties { get; set; }
         public DbSet<Walks> Walks { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            //Preparing data
+            var difficulties = new List<Difficulty>()
+            {
+                new Difficulty()
+                {
+                    Id=Guid.Parse("f5e114fb-7670-4a32-ab4c-8de70c91312a"),
+                    Name="Easy"
+                },
+                new Difficulty()
+                {
+                    Id = Guid.Parse("ed1b493f-e4d3-4d96-9cc6-75c367611469"),
+                    Name = "Medium"
+                },
+                new Difficulty()
+                {
+                    Id=Guid.Parse("46b08cd5-1fa1-493a-aea4-8196b56ac62f"),
+                    Name="Hard"
+                }
+            };
+            //Seed difficulaties to the database
+            modelBuilder.Entity<Difficulty>().HasData(difficulties);
+
+            var regions = new List<Region>()
+            {
+              new Region
+                {
+                    Id = Guid.Parse("f7248fc3-2585-4efb-8d1d-1c555f4087f6"),
+                    Name = "Auckland",
+                    Code = "AKL",
+                    RegionImageUrl = "https://images.pexels.com/photos/5169056/pexels-photo-5169056.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                },
+                new Region
+                {
+                    Id = Guid.Parse("6884f7d7-ad1f-4101-8df3-7a6fa7387d81"),
+                    Name = "Northland",
+                    Code = "NTL",
+                    RegionImageUrl = null
+                },
+                new Region
+                {
+                    Id = Guid.Parse("14ceba71-4b51-4777-9b17-46602cf66153"),
+                    Name = "Bay Of Plenty",
+                    Code = "BOP",
+                    RegionImageUrl = null
+                },
+                new Region
+                {
+                    Id = Guid.Parse("cfa06ed2-bf65-4b65-93ed-c9d286ddb0de"),
+                    Name = "Wellington",
+                    Code = "WGN",
+                    RegionImageUrl = "https://images.pexels.com/photos/4350631/pexels-photo-4350631.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                },
+                new Region
+                {
+                    Id = Guid.Parse("906cb139-415a-4bbb-a174-1a1faf9fb1f6"),
+                    Name = "Nelson",
+                    Code = "NSN",
+                    RegionImageUrl = "https://images.pexels.com/photos/13918194/pexels-photo-13918194.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                },
+                new Region
+                {
+                    Id = Guid.Parse("f077a22e-4248-4bf6-b564-c7cf4e250263"),
+                    Name = "Southland",
+                    Code = "STL",
+                    RegionImageUrl = null
+                }
+            };
+            modelBuilder.Entity<Region>().HasData(regions);
+        }
     }
 }
--- DemoProjectAPI/Migrations/20240724133752_Seed data for Region and Difficulties.Designer.cs
+++ DemoProjectAPI/Migrations/20240724133752_Seed data for Region and Difficulties.Designer.cs
@@ -4,6 +4,7 @@ using DemoProjectAPI.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DemoProjectAPI.Migrations
 {
     [DbContext(typeof(WalksDbContext))]
-    partial class WalksDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240724133752_Seed data for Region and Difficulties")]
+    partial class SeeddataforRegionandDifficulties
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
